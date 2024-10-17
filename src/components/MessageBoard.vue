65fd5848237beba0f3ec878a89f3f752b0ba6d89
Elena TIKHONOVA <elena.tikhonova@elca.ch>
2024-10-17 17:16:11 +0200

    solution

--- src/components/MessageBoard.vue
+++ src/components/MessageBoard.vue
@@ -1,11 +1,21 @@
-<script setup></script>
+<script setup>
+defineProps({
+  messages: {
+    type: Array,
+    required: true
+  }
+})
+
+</script>
 
 <template>
   <div class="message-board">
     <h2 class="message-board-title">Message Board</h2>
-    <ul class="message-board-list">
-      <li></li>
-    </ul>
+    <TransitionGroup tag="ul" name="slide" class="message-board-list">
+      <li v-for="message in messages" :key="message.id">
+        <slot :message="message"></slot>
+      </li>
+    </TransitionGroup>
   </div>
 </template>
 
--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -1,7 +1,26 @@
 <script setup>
 import { faker } from "@faker-js/faker";
+import {ref} from "vue"
+
+const userMessage = ref('')
+
+const emits = defineEmits(['send-msg'])
+
+function sendMessage() {
+  const newMessage = {
+    id: Math.floor(Math.random()).toLocaleString(),
+    author: faker.person.firstName(),
+    content: userMessage.value,
+    timestamp: new Date().toLocaleString(),
+  }
+  emits('send-msg', newMessage)
+  userMessage.value = ''
+}
 </script>
 
 <template>
-  <form class="mt-2"></form>
+  <form class="mt-2" @submit.prevent="sendMessage">
+    <input type="text" class="input-box" v-model="userMessage">
+    <button type="submit" class="send-btn" :disabled="userMessage.length === 0">Submit</button>
+  </form>
 </template>
